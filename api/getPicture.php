ed21a677db92a04ef47e228341df92c05fe6d550
bloudman <adrien@luneau.me>
2017-02-13 08:40:05 +0100

    ajout condition resize

--- api/getPicture.php
+++ api/getPicture.php
@@ -3,8 +3,10 @@ require 'vendor/autoload.php';
 try {
     // Create a new SimpleImage object
     $image = new \claviska\SimpleImage();
-    $image->fromFile('test.jpg');                     // load image.jpg
-    $image->resize($image->getWidth()/2, $image->getHeight()/2);                          // resize to 320x200 pixels
+    $image->fromFile(getcwd() . "/data/Maroc/" . $_GET["file"]);                     // load image.jpg
+    if(!($image->getWidth()<1000 || $image->getHeight() < 1000)) {
+        $image->resize($image->getWidth() / 2, $image->getHeight() / 2);
+    }
     $image->toScreen();                               // output to the screen
 } catch(Exception $err) {
     // Handle errors
